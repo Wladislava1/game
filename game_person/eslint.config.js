16be63a7e07c50c176893ef8f4b89a9135045a8e
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-07-02 13:56:55 +0300

    настройка окружения

--- game_person/eslint.config.js
+++ game_person/eslint.config.js
@@ -1,29 +1,57 @@
-import js from '@eslint/js'
+import babelEslintParser from '@babel/eslint-parser'
+import stylistic from '@stylistic/eslint-plugin'
+import stylisticJsx from '@stylistic/eslint-plugin-jsx'
+import importPlugin from 'eslint-plugin-import'
+import reactPlugin from 'eslint-plugin-react'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ['dist/', 'node_modules/'],
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: babelEslintParser,
       parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
         ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
       },
     },
+    plugins: {
+      '@stylistic': stylistic,
+      '@stylistic/jsx': stylisticJsx,
+      'import': importPlugin,
+      'react': reactPlugin,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/keyword-spacing': 'error',
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/jsx-one-expression-per-line': 'error',
+      '@stylistic/jsx-closing-tag-location': 'error',
+      '@stylistic/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
+      '@stylistic/jsx-max-props-per-line': ['error', { maximum: 1, when: 'multiline' }],
+      '@stylistic/jsx-closing-bracket-location': 'error',
+      '@stylistic/jsx-tag-spacing': 'error',
+      '@stylistic/eol-last': 'error',
+      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+      '@stylistic/semi': ['error', 'never'],
+      'no-console': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'import/extensions': 'off',
     },
   },
-])
+]
